de541f1b1679f317f3f33a0cad12cad00f4fce61
post-coder <gabrielspanu.dev@gmail.com>
2024-04-12 10:09:04 +0200

    feat: controller index function

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts/app')
+
+@section('content')
+    
+<div class="container py-5">
+    <h1>Fumetti</h1>
+
+    @dump($comics)
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Artisti</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+
+            @foreach($comics as $comic)
+            <tr>
+                <td>{{ $comic->title }}</td>
+                <td>{{ $comic->artists }}</td>
+                <td><a href="" class="btn btn-outline-light " >Visualizza</a></td>
+            </tr>
+            @endforeach
+
+        </tbody>
+      </table>
+</div>
+
+@endsection
