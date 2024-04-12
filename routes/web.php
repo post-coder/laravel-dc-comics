98b0bf361b343339a1f8004546fb1cbe89e16361
post-coder <gabrielspanu.dev@gmail.com>
2024-04-12 10:02:43 +0200

    feat: resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+// Route::get('/', function () {
+//     return view('home');
+// })->name('home');
+
+
+// rotta per resource controller
+Route::resource('/comics', ComicController::class);
