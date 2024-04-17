22e396e931261d0e8dd5189dc56e5f23760a2509
post-coder <gabrielspanu.dev@gmail.com>
2024-04-17 09:45:20 +0200

    feat: soft delete on Comic

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comic extends Model
 {
     use HasFactory;
+
+    // qui diciamo che il model (la riga del db) può essere cancellata col soft delete
+    use SoftDeletes;
 }
